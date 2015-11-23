e2d101e4e5c5393807cdc765c5c84c178aa94b4b
awasthi <awasthi@psit.in>
2015-11-23 23:03:29 +0530

    config.php updated.

--- google_login/config.php
+++ google_login/config.php
@@ -7,18 +7,18 @@
 require_once 'messages.php';
 
 //site specific configuration declartion
-define( 'BASE_PATH', 'http://localhost/google_login/');
+define( 'BASE_PATH', 'http://www.darbocite.com/google_login/');
 define( 'DB_HOST', 'localhost' );
 define( 'DB_USERNAME', 'root');
 define( 'DB_PASSWORD', '');
 define( 'DB_NAME', 'user_login');
 
 //Google App Details
-define('GOOGLE_APP_NAME', 'CultivatingHappyness Google+ login');
-define('GOOGLE_OAUTH_CLIENT_ID', 'YOUR CLIENT ID');
-define('GOOGLE_OAUTH_CLIENT_SECRET', 'YOUR CLIENT SECRET');
-define('GOOGLE_OAUTH_REDIRECT_URI', 'YOUR REDIRECT URL');
-define("GOOGLE_SITE_NAME", 'YOUR SITE URL'); 
+define('GOOGLE_APP_NAME', 'Darbocite');
+define('GOOGLE_OAUTH_CLIENT_ID', '1007180659946-gsekk8nj57iio0752d0i0pigehct2l4a.apps.googleusercontent.com');
+define('GOOGLE_OAUTH_CLIENT_SECRET', 'pwqCrmUolHYEEpK88JazK_0V');
+define('GOOGLE_OAUTH_REDIRECT_URI', 'http://www.darbocite.com/google_login');
+define("GOOGLE_SITE_NAME", 'http://www.darbocite.com'); 
 
 function __autoload($class)
 {
