374cbcf312621c6a0e29f2eb71a6732f88e09479
Amit K <awasthi.amitk@gmail.com>
2017-02-14 17:39:47 +0530

    Upadte orcauth index

--- orcauth/index.php
+++ orcauth/index.php
@@ -8,16 +8,16 @@
 <title>Darbocite.</title>
 
 <!-- Bootstrap core CSS -->
-<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="../assets/css/bootstrap.css" rel="stylesheet">
 
 <!-- Custom CSS -->
-<link href="assets/css/main.css" rel="stylesheet">
-<link href="assets/css/font-awesome.min.css" rel="stylesheet">
-<link href="assets/css/animate-custom.css" rel="stylesheet">
+<link href="../assets/css/main.css" rel="stylesheet">
+<link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="../assets/css/animate-custom.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
-<script src="assets/js/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+<script src="../assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="../assets/js/modernizr.custom.js"></script>
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -167,10 +167,10 @@ if (isset($_GET['code'])) {
     ================================================== --> 
 <!-- Placed at the end of the document so the pages load faster --> 
 
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
-<script type="text/javascript" src="assets/js/retina.js"></script> 
-<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script> 
-<script type="text/javascript" src="assets/js/smoothscroll.js"></script> 
-<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="../assets/js/retina.js"></script> 
+<script type="text/javascript" src="../assets/js/jquery.easing.1.3.js"></script> 
+<script type="text/javascript" src="../assets/js/smoothscroll.js"></script> 
+<script type="text/javascript" src="../assets/js/jquery-func.js"></script>
 </body>
 </html>
